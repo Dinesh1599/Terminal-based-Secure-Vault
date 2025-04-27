3e1048aaad2c77e921eed5b275798b91ac1143b7
Dinesh Udayan <dinesh1599@gmail.com>
2025-04-27 13:17:52 -0500

    databaseAuth srcipt added

--- auth.py
+++ auth.py
@@ -1,5 +1,24 @@
 import bcrypt
 from database import create_user, get_user_by_username
 
-def signup:
+def signup(username:str, password:str):
+    user = get_user_by_username(username)
+    if user:
+        print("Username already exists.")
+        return False
+    hashed_pwd = bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+    create_user(username,hashed_pwd)
+    print("Signup Successful")
+    return True
 
+def login(username:str, password:str):
+    user = get_user_by_username(username)
+    if not user:
+        print("User not found.")
+        return None
+    if bcrypt.checkpw(password.encode(), user['password']):
+        print("Login Successful!")
+        return user
+    else:
+        print("Invalid Password.")
+        return None
